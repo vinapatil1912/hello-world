bef787e8cfae16ceecff533c3902f0ee7856c014
vpatil <kokane.vina@gmail.com>
2022-11-21 11:11:51 +0530

    html,css, js file changed

--- MyScript.js
+++ MyScript.js
@@ -6,4 +6,5 @@ document.write("This is the statment written via Javascript");
 let num1=0;
 
 document.write("Welcome to neosoft technology 11- nov 10:51");
-
+let sub=56-34;
+document.write("Subtraction :"+sub);
